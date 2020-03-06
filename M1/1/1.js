83665608a5ad1208e6e3b1c87c5031eff102ed10
Dominik_Czarnas <dominikczarnas@gmail.com>
2020-03-06 16:55:09 +0100

    commit

---
+++ M1/1/1.js
@@ -0,0 +1,9 @@
+// 1) Utwórz funkcję, która jako argument przyjmuje Twój rok urodzenia.
+// Funkcja powinna zwrócić Twój aktualny wiek.
+
+function howOldAreYou (yearOfBirth){
+    const date = new Date().getFullYear();
+    return date - yearOfBirth
+};
+
+console.log(`You are ${howOldAreYou(1991)} years old.`);
