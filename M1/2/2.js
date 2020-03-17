9945a34c412decd2de00b15c58fb5ffa30e757fb
Dominik_Czarnas <dominikczarnas@gmail.com>
2020-03-17 12:37:49 +0100

    commit

--- M1/2/2.js
+++ M1/2/2.js
@@ -4,28 +4,27 @@
 // podanych długości nie można utworzyć trójkąta prostokątnego.
 
 function checkTriangle(a, b, c) {
-    // VALIDATION
-    if (typeof a == "number") {
+  // VALIDATION
+  if (typeof a == "number" && typeof b == "number" && typeof c == "number") {
+    // SORTING DATA
+    let array = [a, b, c].sort(function(a, b) {
+      return a - b;
+    });
+    let a1 = array[0];
+    let b1 = array[1];
+    let c1 = array[2];
 
-        // SORTING DATA
-        let array = [a, b, c].sort(function (a, b) {
-            return a - b
-        })
-        let a1 = array[0]
-        let b1 = array[1]
-        let c1 = array[2]
-
-        // CHECKING CONDITION
-        if (Math.pow(a1, 2) + Math.pow(b1, 2) == Math.pow(c1, 2)) {
-
-            console.log("You can build rectangular triangle from given edges");
-        } else {
-            console.log("You can't build rectangular triangle from given edges");
-        }
+    // CHECKING CONDITION
+    if (Math.pow(a1, 2) + Math.pow(b1, 2) == Math.pow(c1, 2)) {
+      console.log("You can build rectangular triangle from given edges");
+    } else {
+      console.log("You can't build rectangular triangle from given edges");
     }
-    else {
-        console.log("You have to enter 3 numbers to check if the triangle is rectangular");
-    }
+  } else {
+    console.log(
+      "You have to enter 3 numbers to check if the triangle is rectangular"
+    );
+  }
 }
 
 //TEST
