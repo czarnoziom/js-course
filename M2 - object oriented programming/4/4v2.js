9a3956b5efb82b126f8c5765a66211d0bcc863a5
Dominik_Czarnas <dominikczarnas@gmail.com>
2020-04-09 15:02:20 +0200

    commit

--- M2 - object oriented programming/4/4v2.js
+++ M2 - object oriented programming/4/4v2.js
@@ -21,9 +21,8 @@
 // dodawanie nowego użytkownika, usuwanie użytkownika, wypożyczać i zwracać książki
 // znajdujące sięna liście
 
-
-let uuid = require('uuid/v4');
-const moment = require('moment');
+let uuid = require("uuid/v4");
+const moment = require("moment");
 
 class Book {
   constructor(title, author) {
@@ -32,9 +31,11 @@ class Book {
     this.author = author;
     this.status = "available";
   }
+
   changeId(id) {
     this.id = id;
   }
+
   borrow() {
     if (this.status === "available") {
       this.status = "borrowed";
@@ -46,6 +47,7 @@ class Book {
       this.daysOfBorrow = parseInt(difference);
     } else console.log(`Book: '${this.title}' is already borrowed'`);
   }
+
   return() {
     this.dateOfReturn = moment().format("YYYY-MM-DD");
     let start = moment(this.dateOfBorrow);
@@ -53,10 +55,12 @@ class Book {
     // let end = moment(this.dateOfReturn).add(17, "days");  // to check correct counting of penalty add more than 7 days days
     let difference = moment.duration(end.diff(start)).asDays();
     this.daysOfBorrow = parseInt(difference);
-    if(this.daysOfBorrow >7) {
-      this.penalty=(this.daysOfBorrow-7)*0.50+' zł';
-      console.log(`Book: '${this.title}' was kept more than 7 days. Penalty for detention is: ${this.penalty}'`)
-    };
+    if (this.daysOfBorrow > 7) {
+      this.penalty = (this.daysOfBorrow - 7) * 0.5 + " zł";
+      console.log(
+        `Book: '${this.title}' was kept more than 7 days. Penalty for detention is: ${this.penalty}'`
+      );
+    }
   }
 }
 
@@ -79,69 +83,95 @@ class Library {
     this.books = [];
     this.users = [];
   }
-  createUser(name, surname, dateOfBirth, phoneNumber, email) {
+
+  createNewBook(title, author) {
+    let book = new Book(title, author);
+    this.books.push(book);
+  }
+
+  createNewUser(name, surname, dateOfBirth, phoneNumber, email) {
     let user = new User(name, surname, dateOfBirth, phoneNumber, email);
     this.users.push(user);
   }
-  deleteUser(userId) {
-    let userIndex = this.users.findIndex(obj => obj.id === userId);
-    if (userIndex === -1) console.log(`User don't exist`);
-    if (userIndex !== -1){
-      this.users.splice(userIndex,1);
-      console.log(`User deleted successfully`);
-    }
-  }
-  addBooks(...books) {
-    this.books.push(books);
-  }
-  deleteBooks(...books) {
-    for (let i in books) {
-      let index = this.books[0].indexOf(books[i]);
-      if (index !== -1) this.books[0].splice(index, 1);
-    }
-  }
-  borrowBooks(userId, ...books) {
-    for (let i in books) {
-      let userIndex = this.users.findIndex(obj => obj.id === userId);
-      let index = this.books[0].indexOf(books[i]);
-      if (index === -1)
-        console.log(
-          `Book: '${books[i].title}' is already borrowed by someone else`
-        );
-      if (index !== -1) {
-        this.books[0][index].borrow();
-        this.users[userIndex].borrowedBooks.push(this.books[0][index]);
-        console.log(`Book: '${books[i].title}' has been borrowed successfully by user with id: ${this.users[userIndex].id} `);
-      }
-    }
-  }
-  returnBooks(userId, ...books) {
-    let totalPenalty = 0;
-    for (let i in books) {
-      let userIndex = this.users.findIndex(obj => obj.id === userId);
-      let index = this.books[0].indexOf(books[i]);
 
-      if (index === -1)
+  getBookIndexById(id) {
+    let index = this.books.findIndex((obj) => obj.id === id);
+    return index;
+  }
+
+  getUserIndexById(id) {
+    let index = this.users.findIndex((obj) => obj.id === id);
+    return index;
+  }
+
+  deleteBooks(...booksIds) {
+    for (let i in [...booksIds]) {
+      let index = this.books.findIndex((obj) => obj.id === booksIds[i]);
+      if (index === -1) console.log(`Book with id: ${booksIds[i]} don't exist`);
+      if (index !== -1) this.books.splice(index, 1);
+    }
+  }
+
+  deleteUser(...usersIds) {
+    for (let i in [...usersIds]) {
+      let index = this.users.findIndex((obj) => obj.id === usersIds[i]);
+      if (index === -1) console.log(`User with id: ${usersIds[i]} don't exist`);
+      if (index !== -1) this.users.splice(index, 1);
+    }
+  }
+
+  borrowBooks(userId, ...booksIds) {
+    let userIndex = this.getUserIndexById(userId);
+    for (let i in booksIds) {
+      let bookIndex = this.getBookIndexById(booksIds[i]);
+      if (bookIndex === -1)
         console.log(
-          `Book: '${books[i].title}' not found in database. Can't be returned`
+          `Book: '${this.books[bookIndex].title}' is already borrowed by someone else`
         );
-      if (index !== -1) {
-        this.books[0][index].return();
-        if (parseFloat(this.books[0][index].penalty) > 0) totalPenalty += parseFloat(this.books[0][index].penalty);
-        let borrowedBookIndex = this.users[userIndex].borrowedBooks.indexOf(books[i]);
-        this.users[userIndex].borrowedBooks.splice(borrowedBookIndex,1);
-        this.users[userIndex].returnedBooks.push(this.books[0][index]);
-        console.log(`Book: '${books[i].title}' has been returned successfully by user with id: ${this.users[userIndex].id} `);
+      if (bookIndex !== -1) {
+        this.books[bookIndex].borrow();
+        this.users[userIndex].borrowedBooks.push(this.books[bookIndex]);
+        console.log(
+          `Book: '${this.books[bookIndex].title}' has been borrowed successfully by user with id: ${this.users[userIndex].id} `
+        );
       }
     }
-    if (totalPenalty>0)console.log(`Now user have to pay paenalty ${totalPenalty} zł `);
   }
+
+  returnBooks(userId, ...booksIds) {
+    let totalPenalty = 0;
+    let userIndex = this.getUserIndexById(userId);
+    for (let i in booksIds) {
+      let bookIndex = this.getBookIndexById(booksIds[i]);
+      if (bookIndex === -1)
+        console.log(
+          `Book: '${this.books[bookIndex].title}' not found in database. Can't be returned`
+        );
+      if (bookIndex !== -1) {
+        this.books[bookIndex].return();
+        if (parseFloat(this.books[bookIndex].penalty) > 0)
+          totalPenalty += parseFloat(this.book[bookIndex].penalty);
+        let borrowedBookIndex = this.users[userIndex].borrowedBooks.findIndex(
+          (obj) => obj.id === booksIds[i]
+        );
+        this.users[userIndex].borrowedBooks.splice(borrowedBookIndex, 1);
+        this.users[userIndex].returnedBooks.push(this.books[bookIndex]);
+        console.log(
+          `Book: '${this.books[bookIndex].title}' has been returned successfully by user with id: ${this.users[userIndex].id} `
+        );
+      }
+    }
+    if (totalPenalty > 0)
+      console.log(`Now user have to pay paenalty ${totalPenalty} zł `);
+  }
+
   showAvailable() {
     for (let i in this.books[0]) {
       if (this.books[0][i].status === "available")
         console.log(this.books[0][i]);
     }
   }
+
   showBorrowed() {
     for (let i in this.books[0]) {
       if (this.books[0][i].status === "borrowed") console.log(this.books[0][i]);
@@ -151,26 +181,43 @@ class Library {
 
 // TESTS
 let library1 = new Library();
-let book1 = new Book('Harry Potter and the Deathly Hallows', 'J. K. Rowling');
-let book2 = new Book('The Hobbit', 'J. R. R. Tolkien');
-let book3 = new Book('1984', 'George Orwell');
-let book4 = new Book('Pride and Prejudice', 'Jane Austen');
-let book5 = new Book('To Kill a Mockingbird', 'Harper Lee');
-let book6 = new Book('The Da Vinci Code', 'Dan Brown');
-let book7 = new Book('The Great Gatsby', 'F. Scott Fitzgerald');
-let book8 = new Book('Life of Pi', 'Yann Martel');
-let book9 = new Book('The Odyssey', 'Homer');
-let book10 = new Book('Fahrenheit 451', 'Ray Bradbury');
-library1.addBooks(book1,book2,book3,book4,book5,book6,book7,book8,book9,book10);
-library1.deleteBooks(book1, book4, book5, book7);
-library1.createUser('John','Smith','1985','555-555-555','johnsmith@gmail.com');
-library1.users[0].id='111';
-library1.borrowBooks('111', book2,book3,book9);
-library1.returnBooks('111', book2,book3);
-library1.deleteUser('111');
-// library1.showAvailable();
-// library1.showBorrowed();
+library1.createNewBook("Harry Potter and the Deathly Hallows", "J. K. Rowling");
+library1.books[0].id = "000";
+library1.createNewBook('The Hobbit', 'J. R. R. Tolkien');
+library1.books[1].id = "111";
+library1.createNewBook('1984', 'George Orwell');
+library1.books[2].id = "222";
+library1.createNewBook('Pride and Prejudice', 'Jane Austen');
+library1.books[3].id = "333";
+library1.createNewBook('To Kill a Mockingbird', 'Harper Lee');
+library1.books[4].id = "444";
+library1.createNewBook('The Da Vinci Code', 'Dan Brown');
+library1.books[5].id = "555";
+library1.createNewBook('The Great Gatsby', 'F. Scott Fitzgerald');
+library1.books[6].id = "666";
+library1.createNewBook('Life of Pi', 'Yann Martel');
+library1.books[7].id = "777";
+library1.createNewBook('The Odyssey', 'Homer');
+library1.books[8].id = "888";
+library1.createNewBook('Fahrenheit 451', 'Ray Bradbury');
+library1.books[9].id = "999";
 
-// console.log(library1);
+library1.createNewUser("John","Smith","1985","555-555-555","johnsmith@gmail.com");
+library1.users[0].id = "u0";
+library1.createNewUser("John","Doe","1985","555-555-555","johndoe@gmail.com");
+library1.users[1].id = "u1";
+library1.createNewUser("Jane","Doe","1985","555-555-555","janedoe@gmail.com");
+library1.users[2].id = "u2";
+
+library1.deleteBooks('000', '333', '444', '666');
+library1.deleteUser('u0','u2');
+
+library1.borrowBooks('u1', '111','222','999');
+library1.returnBooks('u1', '111','222');
+
+// // library1.showAvailable();
+// // library1.showBorrowed();
+
+console.log(library1);
 // console.log(library1.books);
-console.log(library1.users);
+// console.log(library1.users[0]);
